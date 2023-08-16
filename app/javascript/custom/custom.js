7b6dd21fb22f39c530cd143ba76ba8a20c9a3212
rubyongainz <rubyongainz@gmail.com>
2023-08-16 11:36:38 +0300

    filepond preview has been added

--- app/javascript/custom/custom.js
+++ app/javascript/custom/custom.js
@@ -1,9 +1,13 @@
 import * as FilePond from "filepond";
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
 
 const fileField = document.querySelector(".filepond");
+
+FilePond.registerPlugin(FilePondPluginImagePreview);
 
 const pond = FilePond.create(fileField,{
     credits: {},
     storeAsFile: true,
-    allowMultiple: true
+    allowMultiple: true,
+    allowReorder: true
 });
